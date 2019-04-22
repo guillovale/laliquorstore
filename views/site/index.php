de2f36a9bb276b740bad8b5d90ca9a9285ac88af
guillovale <guillovale@yahoo.com>
2019-04-22 17:54:08 -0500

    make it better

---
+++ views/site/index.php
@@ -0,0 +1,46 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'LIQUOR';
+?>
+<div class="site-index">
+
+
+    <div class="jumbotron">
+	<img src="images/logo.jpg" class="img-thumbnail" alt="Responsive image" width="300" height="200" >
+        <h1>Felicidades!</h1>
+
+        <p class="lead">Usted se encuentra en LA LIQUOR STORE.</p>
+
+        <p><a class="btn btn-lg btn-success" href="#">Proveedor</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                
+
+                <p>.</p>
+
+                <p><a class="btn btn-default" href="#">Documentación &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+               
+
+                <p>.</p>
+
+                <p><a class="btn btn-default" href="#">Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                
+
+                <p>.</p>
+
+                <p><a class="btn btn-default" href="#">Extensiones &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
