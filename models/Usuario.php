055526657d66459907398c9edd3030e62543cecf
guillovale <guillovale@yahoo.com>
2019-05-08 11:34:20 -0500

    productos heroku

--- models/Usuario.php
+++ models/Usuario.php
@@ -135,13 +135,13 @@ class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
      */
     public function validatePassword($password)
     {
-		#echo var_dump(password_verify($password, $this->password)); exit;
-        return password_verify($password, $this->password);
+		#echo var_dump(crypt($password, $this->password), $this->password, hash_equals($this->password, crypt($password, $this->password))); exit;
+        return password_verify($password, trim($this->password));
     }
 	
 	public function setPassword($password)
     {
-        $this->password = password_hash($password, PASSWORD_DEFAULT);
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
     }
 
 }
