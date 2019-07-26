9b5439b5d972d5432a363934732566e908d0dc39
guillovale <guillovale@yahoo.com>
2019-07-25 19:25:25 -0500

    var_dump

--- models/Usuario.php
+++ models/Usuario.php
@@ -135,7 +135,7 @@ class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
      */
     public function validatePassword($password)
     {
-		#echo var_dump(crypt($password, $this->password), $this->password, hash_equals($this->password, crypt($password, $this->password))); exit;
+		echo var_dump(crypt($password, $this->password), $this->password, hash_equals($this->password, crypt($password, $this->password))); exit;
         return password_verify($password, ($this->password));
     }
 	
