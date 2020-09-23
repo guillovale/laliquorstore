2a2dd2003fd064eac1aa7ddc7f71ff9e91c6414e
guillovale <guillovale@yahoo.com>
2020-09-23 12:38:05 -0500

    updated2 web

--- config/web.php
+++ config/web.php
@@ -54,14 +54,12 @@ $config = [
         'db' => $db,
         
         'urlManager' => [
-            'enablePrettyUrl' => true,
+            'enablePrettyUrl' => false,
             #'enableStrictParsing' => true,
             'showScriptName' => false,
-            'rules' => array(
-								'<controller:\w+>/<id:\d+>' => '<controller>/view',
-								'<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-								'<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-						 ),
+            'rules' => [
+            	#['class' => 'yii\rest\UrlRule', 'controller' => 'Productojson'],
+            ],
         ],
         	
     ],
